8d79bb4e7b47cacfd612cc72b937bd0709e2a38e
Michael Martinez <martinez8859@gmail.com>
2019-04-07 16:36:00 +1000

    thanks but no tanks

--- src/games/spirit-game/scenes/game-scene.ts
+++ src/games/spirit-game/scenes/game-scene.ts
@@ -5,8 +5,6 @@
  * @license      Digitsensitive
  */
 
-import { Player } from "../objects/player";
-import { Enemy } from "../objects/enemy";
 import { Ghost } from "../objects/ghost";
 import { Obstacle } from "../objects/obstacles/obstacle";
 import { Hooman } from "../objects/hooman";
@@ -16,7 +14,6 @@ export class GameScene extends Phaser.Scene {
   private tileset: Phaser.Tilemaps.Tileset;
   private layer: Phaser.Tilemaps.StaticTilemapLayer;
 
-  private player: Player;
   private ghost1: Ghost;
   private enemies: Phaser.GameObjects.Group;
   private hoomans: Phaser.GameObjects.Group;
@@ -52,57 +49,8 @@ export class GameScene extends Phaser.Scene {
     this.convertObjects();
 
     // collider layer and obstacles
-    this.physics.add.collider(this.player, this.layer);
-    this.physics.add.collider(this.player, this.obstacles);
     this.physics.add.collider(this.ghost1, this.layer);
     this.physics.add.collider(this.ghost1, this.obstacles);
-
-
-    // collider for bullets
-    this.physics.add.collider(
-      this.player.getBullets(),
-      this.layer,
-      this.bulletHitLayer,
-      null,
-      this
-    );
-
-    this.physics.add.collider(
-      this.player.getBullets(),
-      this.obstacles,
-      this.bulletHitObstacles,
-      null,
-      this
-    );
-
-    this.enemies.children.each((enemy: Enemy) => {
-      this.physics.add.overlap(
-        this.player.getBullets(),
-        enemy,
-        this.playerBulletHitEnemy,
-        null,
-        this
-      );
-      this.physics.add.overlap(
-        enemy.getBullets(),
-        this.player,
-        this.enemyBulletHitPlayer,
-        null
-      );
-
-      this.physics.add.collider(
-        enemy.getBullets(),
-        this.obstacles,
-        this.bulletHitObstacles,
-        null
-      );
-      this.physics.add.collider(
-        enemy.getBullets(),
-        this.layer,
-        this.bulletHitLayer,
-        null
-      );
-    }, this);
 
     this.hoomans.children.each((hooman: Hooman) => {
       this.physics.add.collider(hooman, this.layer, hooman.onHitObstacle, null);
@@ -114,23 +62,7 @@ export class GameScene extends Phaser.Scene {
   }
 
   update(): void {
-    this.player.update();
     this.ghost1.update();
-
-    this.enemies.children.each((enemy: Enemy) => {
-      enemy.update();
-      if (this.player.active && enemy.active) {
-        var angle = Phaser.Math.Angle.Between(
-          enemy.body.x,
-          enemy.body.y,
-          this.player.body.x,
-          this.player.body.y
-        );
-
-        enemy.getBarrel().angle =
-          (angle + Math.PI / 2) * Phaser.Math.RAD_TO_DEG;
-      }
-    }, this);
 
     this.hoomans.children.each((hooman: Hooman) => {
       hooman.update();
@@ -143,26 +75,6 @@ export class GameScene extends Phaser.Scene {
 
     objects.forEach(object => {
       switch (object.type) {
-        case "player": {
-        this.player = new Player({
-          scene: this,
-          x: object.x,
-          y: object.y,
-          key: "tankBlue"
-        });
-        break;
-      }
-      case "enemy": {
-        let enemy = new Enemy({
-          scene: this,
-          x: object.x,
-          y: object.y,
-          key: "tankRed"
-        });
-
-        this.enemies.add(enemy);
-        break;
-      }
       case "ghost1": {
         this.ghost1 = new Ghost({
           scene: this,
@@ -195,23 +107,5 @@ export class GameScene extends Phaser.Scene {
       }
     }
     });
-  }
-
-  private bulletHitLayer(bullet): void {
-    bullet.destroy();
-  }
-
-  private bulletHitObstacles(bullet, obstacle): void {
-    bullet.destroy();
-  }
-
-  private enemyBulletHitPlayer(bullet, player): void {
-    bullet.destroy();
-    player.updateHealth();
-  }
-
-  private playerBulletHitEnemy(bullet, enemy): void {
-    bullet.destroy();
-    enemy.updateHealth();
   }
 }
